bccd2256b0bebf92cf9fd3e7cd2343b39146ce16
Habil khan <habkha905@gmail.com>
2024-04-26 11:46:54 +0200

    Key Binding

--- Typingtest.py
+++ Typingtest.py
@@ -19,7 +19,7 @@ mainframe=Frame(root,bd=4,)
 mainframe.grid()
 
 #creating a title frame inside the main frame
-titleframe=Frame(mainframe,bg='black',bd=4)
+titleframe=Frame(mainframe,bg='gold',bd=4)
 titleframe.grid()
 
 # Adding a title label to the title frame
@@ -57,7 +57,7 @@ textarea_frame=Frame(mainframe)
 textarea_frame.grid(row=2,column=0)
 
 #now adding a texarea in the textarea_fram 
-textarea=Text(textarea_frame,font=('arial', 12,'bold'), width=80,height=15,bd=2,relief=GROOVE,wrap='word',state=DISABLED)
+textarea=Text(textarea_frame,font=('arial', 12,'bold'), width=80,height=6,bd=2,relief=GROOVE,wrap='word',state=DISABLED)
 textarea.grid()
 
 # creating another frame for elapsed time wpm counter and so pn in mainframe
@@ -115,17 +115,152 @@ Exit_button.grid(row=0,column=2,padx=15)
 keyboard_frame=Frame(mainframe)
 keyboard_frame.grid(row=5,column=0)
 
-# creating another frame for keys 1 to 0 inside the keyboard frame and then adding the keys in it seperatly
+# creating another frame for keys 1 to 0 inside the keyboard frame and then adding the keys labels  in it seperatly
+def create_key_label(frame, text):
+    return Label(frame, text=text, bg='black', fg='white', font=('arial', 10, 'bold'), width=5, height=2, bd=10, relief=GROOVE)
+
 key1to0_frame=Frame(keyboard_frame)
-key1to0_frame.grid()
+key1to0_frame.grid(pady=5)
 
-Key_1=Label(key1to0_frame,text='1',bg='black',fg='white',font=('arial',10,'bold'),width=5,height=2,bd=10,relief=GROOVE)
-Key_1.grid()
+# Create labels for keys 1 to 0
+KeyLabel_1 = create_key_label(key1to0_frame, '1')
+KeyLabel_2 = create_key_label(key1to0_frame, '2')
+KeyLabel_3 = create_key_label(key1to0_frame, '3')
+KeyLabel_4 = create_key_label(key1to0_frame, '4')
+KeyLabel_5 = create_key_label(key1to0_frame, '5')
+KeyLabel_6 = create_key_label(key1to0_frame, '6')
+KeyLabel_7 = create_key_label(key1to0_frame, '7')
+KeyLabel_8 = create_key_label(key1to0_frame, '8')
+KeyLabel_9 = create_key_label(key1to0_frame, '9')
+KeyLabel_0 = create_key_label(key1to0_frame, '0')
+
+# Grid the keys 1 to 0
+KeyLabel_1.grid(row=0, column=0, padx=10)
+KeyLabel_2.grid(row=0, column=1, padx=10)
+KeyLabel_3.grid(row=0, column=2, padx=10)
+KeyLabel_4.grid(row=0, column=3, padx=10)
+KeyLabel_5.grid(row=0, column=4, padx=10)
+KeyLabel_6.grid(row=0, column=5, padx=10)
+KeyLabel_7.grid(row=0, column=6, padx=10)
+KeyLabel_8.grid(row=0, column=7, padx=10)
+KeyLabel_9.grid(row=0, column=8, padx=10)
+KeyLabel_0.grid(row=0, column=9, padx=10)
+
+# now creating a frame for  keys Q to Å 
+Frame_QtoA=Frame(keyboard_frame)
+Frame_QtoA.grid(row=1,column=0,pady=5)
+
+# Create labels for keys Q to Å
+KeyLabel_Q = create_key_label(Frame_QtoA, 'Q')
+KeyLabel_W = create_key_label(Frame_QtoA, 'W')
+KeyLabel_E = create_key_label(Frame_QtoA, 'E')
+KeyLabel_R = create_key_label(Frame_QtoA, 'R')
+KeyLabel_T = create_key_label(Frame_QtoA, 'T')
+KeyLabel_Y = create_key_label(Frame_QtoA, 'Y')
+KeyLabel_U = create_key_label(Frame_QtoA, 'U')
+KeyLabel_I = create_key_label(Frame_QtoA, 'I')
+KeyLabel_O = create_key_label(Frame_QtoA, 'O')
+KeyLabel_P = create_key_label(Frame_QtoA, 'P')
+KeyLabel_Å = create_key_label(Frame_QtoA, 'Å')
+
+# Grid the keys Q to Å
+KeyLabel_W.grid(row=0, column=1, padx=9)
+KeyLabel_Q.grid(row=0, column=0, padx=9)
+KeyLabel_E.grid(row=0, column=2, padx=9)
+KeyLabel_R.grid(row=0, column=3, padx=9)
+KeyLabel_T.grid(row=0, column=4, padx=9)
+KeyLabel_Y.grid(row=0, column=5, padx=9)
+KeyLabel_U.grid(row=0, column=6, padx=9)
+KeyLabel_I.grid(row=0, column=7, padx=9)
+KeyLabel_O.grid(row=0, column=8, padx=9)
+KeyLabel_Å.grid(row=0, column=9, padx=9)
 
 
+# Creating frame for Keys A-Ä 
+Frame_AtoÄ=Frame(keyboard_frame)
+Frame_AtoÄ.grid(row=2,column=0,pady=5)
 
 
+# Create labels for keys A to Ä
+KeyLabel_A = create_key_label(Frame_AtoÄ, 'A')
+KeyLabel_S = create_key_label(Frame_AtoÄ, 'S')
+KeyLabel_D = create_key_label(Frame_AtoÄ, 'D')
+KeyLabel_F = create_key_label(Frame_AtoÄ, 'F')
+KeyLabel_G = create_key_label(Frame_AtoÄ, 'G')
+KeyLabel_H = create_key_label(Frame_AtoÄ, 'H')
+KeyLabel_J = create_key_label(Frame_AtoÄ, 'J')
+KeyLabel_K = create_key_label(Frame_AtoÄ, 'K')
+KeyLabel_L = create_key_label(Frame_AtoÄ, 'L')
+KeyLabel_Ö = create_key_label(Frame_AtoÄ, 'Ö')
+KeyLabel_Ä = create_key_label(Frame_AtoÄ, 'Ä')
 
+# Grid the keys A to Ä
+KeyLabel_A.grid(row=0, column=0, padx=5)
+KeyLabel_S.grid(row=0, column=1, padx=5)
+KeyLabel_D.grid(row=0, column=2, padx=5)
+KeyLabel_F.grid(row=0, column=3, padx=5)
+KeyLabel_G.grid(row=0, column=4, padx=5)
+KeyLabel_H.grid(row=0, column=5, padx=5)
+KeyLabel_J.grid(row=0, column=6, padx=5)
+KeyLabel_K.grid(row=0, column=7, padx=5)
+KeyLabel_L.grid(row=0, column=8, padx=5)
+KeyLabel_Ö.grid(row=0, column=9, padx=5)
+KeyLabel_Ä.grid(row=0, column=10, padx=5)
+
+
+#Now doing the same thing last time with keys z to m
+Frame_ZtoM=Frame(keyboard_frame)
+Frame_ZtoM.grid(row=3,column=0,pady=5)
+
+
+# Create labels for keys Z to M
+KeyLabel_Z = create_key_label(Frame_ZtoM, 'Z')
+KeyLabel_X = create_key_label(Frame_ZtoM, 'X')
+KeyLabel_C = create_key_label(Frame_ZtoM, 'C')
+KeyLabel_V = create_key_label(Frame_ZtoM, 'V')
+KeyLabel_B = create_key_label(Frame_ZtoM, 'B')
+KeyLabel_N = create_key_label(Frame_ZtoM, 'N')
+KeyLabel_M = create_key_label(Frame_ZtoM, 'M')
+
+# Grid the keys Z to M
+KeyLabel_Z.grid(row=0, column=0, padx=8)
+KeyLabel_X.grid(row=0, column=1, padx=8)
+KeyLabel_C.grid(row=0, column=2, padx=8)
+KeyLabel_V.grid(row=0, column=3, padx=8)
+KeyLabel_B.grid(row=0, column=4, padx=8)
+KeyLabel_N.grid(row=0, column=5, padx=8)
+KeyLabel_M.grid(row=0, column=6, padx=8)
+
+# Creating a frame for the space button and adding in the  The button
+Spacekey_Frame=Frame(keyboard_frame)
+Spacekey_Frame.grid(row=4,column=0,pady=3)
+
+
+Spacekey=Label(Spacekey_Frame,text='Space',bg='black',fg='white',font=('arial',10,'bold'),width=30,height=2,bd=10,relief=GROOVE)
+Spacekey.grid()
+
+
+def changeBG(widget):
+    widget.config(bg='red')
+    widget.after(500,lambda:widget.config(bg='black'))
+# Binding my keyboard keys with the keys i just created so u can see what key i clivk on 
+
+#NOW creating 3  list and adding all the numbers and alphabets  and space   labels in it 
+NumberzLabel_list=[KeyLabel_1,KeyLabel_2,KeyLabel_3,KeyLabel_4,KeyLabel_5,KeyLabel_6,KeyLabel_7,KeyLabel_8,KeyLabel_9,KeyLabel_0]
+Albhabetslabel_list=[KeyLabel_A,KeyLabel_B,KeyLabel_C,KeyLabel_D,KeyLabel_E,KeyLabel_F,KeyLabel_G,KeyLabel_H,KeyLabel_I,KeyLabel_J,KeyLabel_K,KeyLabel_L,KeyLabel_M,KeyLabel_N,KeyLabel_O,KeyLabel_P,KeyLabel_Q,KeyLabel_R,KeyLabel_S,KeyLabel_T,KeyLabel_U,KeyLabel_V,KeyLabel_W,KeyLabel_Y,KeyLabel_Z,KeyLabel_Å,KeyLabel_Ä,KeyLabel_Ö]
+Space_list=[Spacekey]
+
+# now creating  numbers and alphabets that that my labe labove will binded WITH
+
+Binding_numbers=['1','2','3','4','5','6','7','8','9','0']
+Binding_alabhabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+Bindingcap_alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Å','Å','Ö'],
+
+for numbers in range(len(Binding_numbers)):
+    root.bind(Binding_numbers[numbers], lambda event, label=NumberzLabel_list[numbers]: changeBG(label))
+
+
+for captil_alphabets in range(len(Bindingcap_alphabet)):
+    root.bind(Bindingcap_alphabet[captil_alphabets], lambda event, label=Albhabetslabel_list[captil_alphabets]: changeBG(label))
 
 root.mainloop()
-
